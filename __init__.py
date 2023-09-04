8d6422146676bc7ddcd8eff2b79fe5a883c50776
SydrrO <129657153+SydrrO@users.noreply.github.com>
2023-09-04 15:31:36 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,6 +15,10 @@ def url_searching(url_pool: list):
                 target=get_url, args=(url,)
             )
         )
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
 
 
 # Press the green button in the gutter to run the script.
@@ -23,4 +27,4 @@ if __name__ == '__main__':
         f'www.buff/goods/{index}.com'
         for index in range(36000, 37000)
     ]
-    url_searching(urls=urls)
+    url_searching(url_pool=urls)
